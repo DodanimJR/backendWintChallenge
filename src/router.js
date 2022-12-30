5e49adc1a718e10c6fc6232f23fab06adfba4be9
Dodanim <dodanimjunior24@gmail.com>
2022-12-30 13:39:30 -0600

    first structure of routing semi complete!

---
+++ src/router.js
@@ -0,0 +1,22 @@
+var { expressjwt: jwt } = require("express-jwt");
+const express = require('express');
+const authRouter = require('./auth/router');
+const userRouter = require('./user/router');
+const budgetRouter = require('./budget/router');
+const categoryRouter = require('./category/router');
+const projectRouter = require('./project/router');
+const transactionRouter = require('./transaction/router');
+
+
+
+
+
+const router = express.Router();
+
+router.use(express.json())
+
+
+
+
+
+module.exports=router;
